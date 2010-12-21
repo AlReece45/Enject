5943bb5a322313670f0d0d6a37a81a2e7b891bf6
Alexander Reece <AlReece45@gmail.com>
2010-12-21 06:19:51 -0600

    Update (and implement) Test_Enject_Injector_Mock

--- dev/PHPUnit/Enject/Injector/Mock.php
+++ dev/PHPUnit/Enject/Injector/Mock.php
@@ -1,11 +1,46 @@
 <?php
+/*
+ * Enject Library Tests
+ * Copyright 2010 Alexander Reece
+ * Licensed under: GNU Lesser Public License 2.1 or later
+ *//**
+ * @author Alexander Reece <alreece45@gmail.com>
+ * @copyright 2010 (c) Alexander Reece
+ * @license http://www.opensource.org/licenses/lgpl-2.1.php
+ * @package Test_Enject
+ */
+require_once 'Enject/Injector.php';
 
-	require_once 'Enject/Injector.php';
-
-	class Test_Enject_MockInjector
-		implements Enject_Injector
+/*
+ * Helps test the and verify the injection functionality of
+ * {@link Enject_Container}
+ */
+class Test_Enject_Injector_Mock
+	implements Enject_Injector
+{
+	/**
+	 * Constructor
+	 */
+	function __construct()
 	{
-		function inject($container, $object)
-		{
-		}		
+		$this->_injectedObjects = new SplObjectStorage();
 	}
+
+	/**
+	 * @param Enject_Container $container
+	 * @param Object $object
+	 */
+	function inject($container, $object)
+	{
+		$this->_injectedObjects->attach($object);
+	}
+
+	/**
+	 * @param Object $object
+	 * @return Boolean
+	 */
+	function isObjectInjected($object)
+	{
+		return $this->_injectedObjects->contains($object);
+	}
+}
