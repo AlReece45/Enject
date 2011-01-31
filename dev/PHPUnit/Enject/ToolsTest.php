b9bdddc4dc47050e45bb76e9df0f2bf2a31735e2
Alexander Reece <AlReece45@gmail.com>
2011-01-31 07:03:29 -0600

    Add copyright header to Enject_Tools test

--- dev/PHPUnit/Enject/ToolsTest.php
+++ dev/PHPUnit/Enject/ToolsTest.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * Enject Library
+ * Copyright 2010 Alexander Reece
+ * Licensed under: GNU Lesser Public License 2.1 or later
+ *//**
+ * @author Alexander Reece <alreece45@gmail.com>
+ * @copyright 2010 (c) Alexander Reece
+ * @license http://www.opensource.org/licenses/lgpl-2.1.php
+ * @package Test_Enject
+ */
 
 require_once 'PHPUnit/Framework/TestCase.php';
 require_once 'Enject/TestCase.php';
